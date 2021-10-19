e8fdad12dbafac271fba1066d537c234285ed1df
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-10-19 13:50:25 +0530

    Added: prime number check

---
+++ DayPrograms/src/Com/Bridgelabz/Day1/PrimeNumber.java
@@ -0,0 +1,31 @@
+package Com.Bridgelabz.Day1;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) 
+	{
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter the number here");
+	    int num = sc.nextInt();
+        
+	    int count=0;
+	    for(int i = 2;i<num;i++)
+	    {
+	    	if(num % i == 0)
+	    	{
+	    		count++;
+	    	}
+	    }
+	    if(count == 0)
+	    {
+	    	System.out.println(num+" : is a prime");
+	    }
+	    else
+	    {
+	    	System.out.println(num+ ": is not a prime");
+	    }
+	}
+
+}
